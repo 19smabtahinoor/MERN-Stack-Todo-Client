2ab6883cb3fbdaedbdc70f62e35f76df6db8c78d
S.M.ABTAHI NOOR <abtahinoorkabid@gmail.com>
2021-10-24 01:14:49 +0600

    UI Changed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -13,7 +13,6 @@ const HomeScreen = () => {
     }, [todos])
 
     //delete 
-
     const handleDelete = (id) => {
         const process = window.confirm('Are you sure, Do you want to delete ? ')
         if (process) {
@@ -32,15 +31,33 @@ const HomeScreen = () => {
     }
     return (
         <section className="max-w-screen-lg mx-auto py-24 px-6">
-            <h1 className="text-3xl text-center pb-4">All Todos</h1>
+            <h1 className="text-3xl text-center pb-4">All Todos ({todos.length}) </h1>
+
+            {/* headers option  */}
+            <div className="p-4 my-4 box-border rounded-lg border border-gray-200 flex items-center">
+                {/* search  */}
+                <div className="flex flex-grow">
+                    <input type="text" placeholder="Search" className="px-6 py-3 border border-gray-200 ring-purple-300 focus:ring-4 focus:outline-none transition duration-300 w-96 rounded-lg" />
+                </div>
+                {/* filter  */}
+                <div className="flex items-center space-x-3">
+                    <span className="text-gray-700">Filter by Status :</span>
+                    <select className=" border border-gray-200 p-3 rounded-lg focus:outline-none focus:ring-4 ring-purple-300 transition duration-300">
+                        <option defaultValue="Planning todo" className="text-lg py-6 rounded-lg">Planning todo</option>
+                        <option value="In Progress" className="text-lg py-6  rounded-lg ">In Progress</option>
+                        <option value="Done" className="text-lg py-6  rounded-lg">Done</option>
+                    </select>
+                </div>
+            </div>
+
             <div className="flex flex-col space-y-6">
                 {todos?.map((todo) => (
                     <div className="flex items-center w-full shadow-lg rounded-lg  border border-gray-200 p-4 box-border bg-white" key={todo._id}>
-                        <div className="flex flex-grow items-center w-96 flex-wrap">
+                        <div className="flex items-center w-96">
                             <h1 className="text-xl font-semibold break-all">{todo.todo}</h1>
                         </div>
                         {/* category  */}
-                        <div className="flex flex-grow pr-6">
+                        <div className="flex flex-grow pr-6 w-24">
                             <span className="text-gray-700 text-sm break-all">{todo.description}</span>
                         </div>
                         {/* status  */}
