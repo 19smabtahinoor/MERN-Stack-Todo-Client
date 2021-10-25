36537e8899abd4496881502d8bd78e89ca506f47
S.M.ABTAHI NOOR <abtahinoorkabid@gmail.com>
2021-10-25 11:49:40 +0600

    Filtering System Done

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,8 @@ import { FiEdit } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 const HomeScreen = () => {
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([]);
+    const [status, setStatus] = useState('All');
 
     useEffect(() => {
         fetch('https://mern-todo-sm.herokuapp.com/todos')
@@ -29,6 +30,12 @@ const HomeScreen = () => {
                 })
         }
     }
+
+    //handle filtering 
+    const handleChange = (e) => {
+        setStatus(e.target.value)
+    }
+
     return (
         <section className="max-w-screen-lg mx-auto py-24 px-6">
             <h1 className="text-3xl text-center pb-4">All Todos ({todos.length}) </h1>
@@ -42,8 +49,9 @@ const HomeScreen = () => {
                 {/* filter  */}
                 <div className="flex items-center space-x-3">
                     <span className="text-gray-700">Filter by Status :</span>
-                    <select className=" border border-gray-200 p-3 rounded-lg focus:outline-none focus:ring-4 ring-purple-300 transition duration-300">
-                        <option defaultValue="Planning todo" className="text-lg py-6 rounded-lg">Planning todo</option>
+                    <select className=" border border-gray-200 p-3 rounded-lg focus:outline-none focus:ring-4 ring-purple-300 transition duration-300" value={status} onChange={handleChange}>
+                        <option value="All" className="text-lg py-6 rounded-lg">All</option>
+                        <option value="Planning todo" className="text-lg py-6 rounded-lg">Planning todo</option>
                         <option value="In Progress" className="text-lg py-6  rounded-lg ">In Progress</option>
                         <option value="Done" className="text-lg py-6  rounded-lg">Done</option>
                     </select>
@@ -51,29 +59,57 @@ const HomeScreen = () => {
             </div>
 
             <div className="flex flex-col space-y-6">
-                {todos?.map((todo) => (
-                    <div className="flex items-center w-full shadow-lg rounded-lg  border border-gray-200 p-4 box-border bg-white" key={todo._id}>
-                        <div className="flex items-center w-96">
-                            <h1 className="text-xl font-semibold break-all">{todo.todo}</h1>
-                        </div>
-                        {/* category  */}
-                        <div className="flex flex-grow pr-6 w-24">
-                            <span className="text-gray-700 text-sm break-all">{todo.description}</span>
-                        </div>
-                        {/* status  */}
-                        <div className="flex flex-grow mr-4 rounded-md">
-                            <p className={`text-center text-white px-3 py-1 rounded-full text-sm ${todo.status === "In Progress" ? "bg-red-600" : "bg-yellow-600"}  ${todo.status === "Done" && "bg-green-700"}`}>{todo.status}</p>
-                        </div>
-                        <div className="flex space-x-3">
-                            <Link to={`/edit/${todo._id}`}>
-                                <FiEdit className="cursor-pointer text-green-600 text-2xl" />
-                            </Link>
-                            <AiOutlineDelete className="cursor-pointer text-2xl text-red-500" onClick={() => handleDelete(todo._id)} />
-                        </div>
-                    </div>
-                ))}
+                {status === 'All' ? (
+                    <>
+                        {todos?.map((todo) => (
+                            <div className="flex items-center w-full shadow-lg rounded-lg  border border-gray-200 p-4 box-border bg-white" key={todo._id}>
+                                <div className="flex items-center w-96">
+                                    <h1 className="text-xl font-semibold break-all">{todo.todo}</h1>
+                                </div>
+                                {/* category  */}
+                                <div className="flex flex-grow pr-6 w-24">
+                                    <span className="text-gray-700 text-sm break-all">{todo.description}</span>
+                                </div>
+                                {/* status  */}
+                                <div className="flex flex-grow mr-4 rounded-md">
+                                    <p className={`text-center text-white px-3 py-1 rounded-full text-sm ${todo.status === "In Progress" ? "bg-red-600" : "bg-yellow-600"}  ${todo.status === "Done" && "bg-green-700"}`}>{todo.status}</p>
+                                </div>
+                                <div className="flex space-x-3">
+                                    <Link to={`/edit/${todo._id}`}>
+                                        <FiEdit className="cursor-pointer text-green-600 text-2xl" />
+                                    </Link>
+                                    <AiOutlineDelete className="cursor-pointer text-2xl text-red-500" onClick={() => handleDelete(todo._id)} />
+                                </div>
+                            </div>
+                        ))}
+                    </>
+                ) : (
+                    <>
+                        {todos.filter(item => item.status === status)?.map((todo) => (
+                            <div className="flex items-center w-full shadow-lg rounded-lg  border border-gray-200 p-4 box-border bg-white" key={todo._id}>
+                                <div className="flex items-center w-96">
+                                    <h1 className="text-xl font-semibold break-all">{todo.todo}</h1>
+                                </div>
+                                {/* category  */}
+                                <div className="flex flex-grow pr-6 w-24">
+                                    <span className="text-gray-700 text-sm break-all">{todo.description}</span>
+                                </div>
+                                {/* status  */}
+                                <div className="flex flex-grow mr-4 rounded-md">
+                                    <p className={`text-center text-white px-3 py-1 rounded-full text-sm ${todo.status === "In Progress" ? "bg-red-600" : "bg-yellow-600"}  ${todo.status === "Done" && "bg-green-700"}`}>{todo.status}</p>
+                                </div>
+                                <div className="flex space-x-3">
+                                    <Link to={`/edit/${todo._id}`}>
+                                        <FiEdit className="cursor-pointer text-green-600 text-2xl" />
+                                    </Link>
+                                    <AiOutlineDelete className="cursor-pointer text-2xl text-red-500" onClick={() => handleDelete(todo._id)} />
+                                </div>
+                            </div>
+                        ))}
+                    </>
+                )}
             </div>
-        </section>
+        </section >
     )
 }
 
